19dd170c8a1ab31e55026adf97a2961f7ab517d3
chriseagle1 <christemp@163.com>
2022-05-22 00:57:35 +0800

    todolist开发

---
+++ todolist/src/store/index.js
@@ -0,0 +1,37 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    list: [
+      {
+          title: '吃饭',
+          complete: false
+      },
+      {
+          title: '睡觉',
+          complete: false
+      },
+      {
+          title: '搬砖',
+          complete: true
+      }
+    ]
+  },
+  getters: {
+  },
+  mutations: {
+    addTask(state, payload) {
+      state.list.push(payload)
+    },
+    delTask(state, payload) {
+      state.list.splice(payload, 1)
+    },
+    resetList(state, payload) {
+      state.list = payload.
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
